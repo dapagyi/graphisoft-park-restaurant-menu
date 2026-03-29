912c0ea4351c878f1dc5b3585aa7b231f7c10d61
David Apagyi <apagyi.david@gmail.com>
2026-03-29 17:05:47 +0200

    feat: add scheduler

---
+++ src/graphisoft_park_restaurant_menu/main.py
@@ -0,0 +1,56 @@
+import os
+
+import structlog
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from dotenv import load_dotenv
+
+from graphisoft_park_restaurant_menu.menu_scraper import scrape_menu
+from graphisoft_park_restaurant_menu.notification import send_menu_to_slack
+
+load_dotenv()
+log = structlog.get_logger()
+
+
+def main() -> None:
+    is_slack_configured = (
+        os.getenv("SLACK_BOT_TOKEN") and os.getenv("SLACK_CHANNEL")
+    ) or False
+    if not is_slack_configured:
+        log.warning(
+            "Slack configuration is missing. Notifications will not be sent to Slack."
+        )
+
+    timezone = os.getenv("TIMEZONE", "Europe/Budapest")
+    scheduler = BlockingScheduler(timezone=timezone)
+    trigger = CronTrigger.from_crontab(
+        os.getenv("NOTIFICATION_SCHEDULE", "0 11 * * mon-fri")
+    )
+
+    @scheduler.scheduled_job(trigger)
+    def _send_menu_to_slack_job() -> None:
+        try:
+            menu = scrape_menu()
+            log.debug("Menu scraped successfully", menu=menu)
+            if is_slack_configured:
+                response = send_menu_to_slack(menu)
+                log.debug("Menu sent to Slack successfully", response=response)
+
+        except Exception as e:
+            log.error("Error occurred while scraping menu", error=e)
+
+    try:
+        log.info(
+            "Starting scheduler with trigger: %s",
+            trigger,
+            timezone=timezone,
+            is_slack_configured=is_slack_configured,
+        )
+        scheduler.start()
+    except KeyboardInterrupt:
+        log.info("Stopping scheduler")
+        scheduler.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- src/graphisoft_park_restaurant_menu/notification.py
+++ src/graphisoft_park_restaurant_menu/notification.py
@@ -3,13 +3,14 @@ from pprint import pprint
 
 from dotenv import load_dotenv
 from slack_sdk import WebClient
+from slack_sdk.web import SlackResponse
 
 from graphisoft_park_restaurant_menu.menu_scraper import Category, scrape_menu
 
 load_dotenv()
 
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+SLACK_CHANNEL = os.getenv("SLACK_CHANNEL")
 
 
 def build_slack_message(menu: list[Category]) -> list:
@@ -28,18 +29,19 @@ def build_slack_message(menu: list[Category]) -> list:
     ]
 
 
-def send_menu_to_slack(menu: list[Category]) -> None:
+def send_menu_to_slack(menu: list[Category]) -> SlackResponse:
     blocks = build_slack_message(menu)
     pprint(blocks)
 
     client = WebClient(token=SLACK_BOT_TOKEN)
-    client.chat_postMessage(
+    response = client.chat_postMessage(
         channel=SLACK_CHANNEL,
         blocks=blocks,
         username="Kör Étterem",
         icon_emoji="fork_and_knife",
         text="Check out today's menu!",
     )
+    return response
 
 
 if __name__ == "__main__":
